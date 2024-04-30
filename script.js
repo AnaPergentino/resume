7dae37f66285a50f02d340f011faa244a5617cf7
Ana Carolina dos Santos Pergentino <anacpergentino@hotmail.com>
2024-04-30 17:57:06 -0300

    new viausl features

--- script.js
+++ script.js
@@ -8,28 +8,50 @@ document.addEventListener('DOMContentLoaded', () => {
   const slideInAnimation = () => {
     header.style.opacity = '1';
     header.style.transform = 'translateY(0)';
-
+/*
     mainLeft.style.opacity = '1';
     mainLeft.style.transform = 'translateX(0)';
 
     mainRight.style.opacity = '1';
-    mainRight.style.transform = 'translateX(0)';
+    mainRight.style.transform = 'translateX(0)';*/
   };
 
   // Trigger the animation when the page is fully loaded
   slideInAnimation();
+  document.addEventListener("mousemove", rotateCube)
 
 });
-  // Selection of HTML objects
-  const burger = document.querySelector('.burger i');
-  const nav = document.querySelector('.nav');
-  // Defining a function
-  function toggleNav() {
-    burger.classList.toggle('fa-bars');
-    burger.classList.toggle('fa-times');
-    nav.classList.toggle('nav-active');
+// Selection of HTML objects
+const burger = document.querySelector('.burger i');
+const nav = document.querySelector('.nav');
+// Defining a function
+function toggleNav() {
+  nav.classList.toggle('nav-collapsed');
+
+  burger.classList.toggle('fa-bars');
+  burger.classList.toggle('fa-times');
+  nav.classList.toggle('nav-active');
+
+}
+// Calling the function after click event occurs
+burger.addEventListener('click', function () {
+  toggleNav();
+});
+burger.addEventListener('touchstart', () => { });
+burger.addEventListener('touchend', () => { });
+burger.addEventListener('touchcancel', () => { });
+burger.addEventListener('touchmove', () => { });
+
+function rotateCube(e) {
+  var cube = document.getElementsByClassName("cube");
+  var x = e.clientX - window.innerWidth / 2;
+  var y = e.clientY - window.innerHeight / 2;
+  var q = 0.15;
+  var i;
+
+  x = x * q * 1.25;
+  y = -y * q * 1.25;
+  for (i = 0; i < cube.length; i++) {
+    cube[i].style.transform = "rotateY(" + x + "deg) rotateX(" + y + "deg)"
   }
-  // Calling the function after click event occurs
-  burger.addEventListener('click', function () {
-    toggleNav();
-  });
+}
