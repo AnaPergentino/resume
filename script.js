0984a28925565a34a990e5640bef19af49a54337
Ana Carolina dos Santos Pergentino <anacpergentino@hotmail.com>
2024-05-15 19:16:15 -0300

    Fixes navbar

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
   // Select elements to animate
-  const header = document.querySelector('h1');
-  const mainLeft = document.querySelector('#mainLeft');
+  const title = document.querySelector('#main-title');
+  const subtitle = document.querySelector('#main-subtitle');
   const mainRight = document.querySelector('#mainRight');
 
   // Function to handle the slide-in animation
   const slideInAnimation = () => {
-    header.style.opacity = '1';
-    header.style.transform = 'translateY(0)';
-/*
-    mainLeft.style.opacity = '1';
-    mainLeft.style.transform = 'translateX(0)';
+    title.style.opacity = '1';
+    title.style.transform = 'translateY(0)';
+
+    subtitle.style.opacity = '1';
+    subtitle.style.transform = 'translateY(0)';
 
     mainRight.style.opacity = '1';
-    mainRight.style.transform = 'translateX(0)';*/
+    mainRight.style.transform = 'translateX(0)';
   };
 
   // Trigger the animation when the page is fully loaded
   slideInAnimation();
-  document.addEventListener("mousemove", rotateCube)
 
 });
 // Selection of HTML objects
@@ -42,16 +41,3 @@ burger.addEventListener('touchend', () => { });
 burger.addEventListener('touchcancel', () => { });
 burger.addEventListener('touchmove', () => { });
 
-function rotateCube(e) {
-  var cube = document.getElementsByClassName("cube");
-  var x = e.clientX - window.innerWidth / 2;
-  var y = e.clientY - window.innerHeight / 2;
-  var q = 0.15;
-  var i;
-
-  x = x * q * 1.25;
-  y = -y * q * 1.25;
-  for (i = 0; i < cube.length; i++) {
-    cube[i].style.transform = "rotateY(" + x + "deg) rotateX(" + y + "deg)"
-  }
-}
